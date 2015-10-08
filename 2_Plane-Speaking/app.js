835cb978dda90f181d86afda18a39defbdd8ff3d
mirman-fiweeksmanjarres2016 <fiweeksmanjarres2016@mirmanmustangs.org>
2015-10-08 12:09:23 -0700

    Added Pictures

--- 2_Plane-Speaking/app.js
+++ 2_Plane-Speaking/app.js
@@ -1,18 +1,20 @@
-var xPlanes = [
-  "X-1",
-  "X-3",
-  "X-15",
-  "X-29",
-  "X-31",
-  "X-37",
-  "X-45"
-];
+var xPlanes = {
+  "X-1" : "First aircraft to break the sound barrier while flying level",
+  "X-3" : "Testing for long duration high speed flights",
+  "X-15" : "First manned hypersonic flight",
+  "X-29" : "Forward-swept wing testbed",
+  "X-31" : "Thrust vectoring supermaneuverability",
+  "X-37" : "Reusible Earth Orbiting Spacecraft",
+  "X-45" : "UCAV (unmanned combat air vehicle) demonstrator"
+};
 
 // Now that we have our array of planes, we need to add elements to our DOM
 // for each one.
-var e = document. //This line is unfinished! Use a method to capture our <ul> element.
+//This line is unfinished! Use a method to capture our <ul> element.
+var e = document.getElementById("planes-list");
 
-for(var i=0; i<xPlanes.length; i++) {
-  var xPlane = xPlanes[i]; //What does "i" refer to?
-  // Do some things to add <li> elements to our empty list.
+for (var s in xPlanes) {
+e.innerHTML += "<div><h2>" + s + "</h2>" ;
+e.innerHTML += "<p>" + xPlanes[s] + "</p>"
+e.innerHTML += "<img src= 'Pictures/" + s + ".jpg' style = 'width:25%;height:25%'></div>"
 }
