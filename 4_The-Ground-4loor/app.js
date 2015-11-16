82be5437d48eff16e498dc5a0c105314858f85c8
mirman-fiweeksmanjarres2016 <fiweeksmanjarres2016@mirmanmustangs.org>
2015-11-16 14:39:32 -0800

    Added Menu Bar

--- 4_The-Ground-4loor/app.js
+++ 4_The-Ground-4loor/app.js
@@ -1,11 +1,12 @@
-$(document).ready(function(){
-  $("#contact-form").submit(function(event){
-  //  event.preventDefault();
-    //Use jQuery to collect the values from the fields and create a
-    //JSON object containing them. Then, log the result to the console
-
-    // Secondary Objective: Use the AJAX methods built into jQuery to send
-    // The form data to http://x.mirman.org:9999. Write a callback to handle
-    // the response.
-  })
+$(document).on( "submit", function( event ) {
+  event.preventDefault();
+  
+  console.log( $("#firstname").serialize() );
+  console.log( $("#lastname").serialize() );
+  console.log( $("#email").serialize() );
+  console.log( $("#message").serialize() );
 });
+
+var destinationUrl = "http://x.mirman.org:1031/ground4loor";
+$.post(destinationUrl, formData, function(response){
+  console.log(response);
