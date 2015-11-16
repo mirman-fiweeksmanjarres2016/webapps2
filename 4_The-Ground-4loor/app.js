e7df076dfb6a11a0b6f87e840d2773c2493f1b8d
mirman-fiweeksmanjarres2016 <fiweeksmanjarres2016@mirmanmustangs.org>
2015-11-16 15:07:12 -0800

    Work on 3 and 4

--- 4_The-Ground-4loor/app.js
+++ 4_The-Ground-4loor/app.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 $(document).on( "submit", function( event ) {
   event.preventDefault();
   
@@ -10,3 +11,16 @@ $(document).on( "submit", function( event ) {
 var destinationUrl = "http://x.mirman.org:1031/ground4loor";
 $.post(destinationUrl, formData, function(response){
   console.log(response);
+=======
+$(document).ready(function(){
+  $("#contact-form").submit(function(event){
+    event.preventDefault();
+    //Use jQuery to collect the values from the fields and create a
+    //JSON object containing them. Then, log the result to the console
+
+    // Secondary Objective: Use the AJAX methods built into jQuery to send
+    // The form data to http://x.mirman.org:9999. Write a callback to handle
+    // the response.
+  })
+});
+>>>>>>> 67e65d0c79640b847b1ea6ba50cd8b4fab084578
